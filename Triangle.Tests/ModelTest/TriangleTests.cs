dc8ea5817f327a6e84dbf8bd37695b6dcdb0955b
Sean Downs <spdowns@protonmail.com>
2020-07-14 11:39:19 -0700

    add third test to return a false bool if sides don't make a triangle. FAIL
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- Triangle.Tests/ModelTest/TriangleTests.cs
+++ Triangle.Tests/ModelTest/TriangleTests.cs
@@ -19,5 +19,12 @@ namespace Triangle.Tests
       Triangle testTriangle = new Triangle(3, 4, 5);
       Assert.AreEqual(true, testTriangle.IsValid());
     }
+
+    [TestMethod]
+    public void IsValid_CheckIfTriangleIsInvalid_False()
+    {
+      Triangle testTriangle = new Triangle(2, 2, 8);
+      Assert.AreEqual(false, testTriangle.IsValid());
+    }
   }
 }
