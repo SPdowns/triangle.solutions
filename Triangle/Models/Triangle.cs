8f4e5782e86309dd4d25a44d6d0187d3df354556
Sean Downs <spdowns@protonmail.com>
2020-07-14 11:34:49 -0700

    changed twelve characters in the IsValid property. PASS
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- Triangle/Models/Triangle.cs
+++ Triangle/Models/Triangle.cs
@@ -5,5 +5,17 @@ namespace Triangle
     public int SideOne { get; set; }
     public int SideTwo { get; set; }
     public int SideThree { get; set; }
+
+    public Triangle(int sideOne, int sideTwo, int sideThree)
+    {
+      SideOne = sideOne;
+      SideTwo = sideTwo;
+      SideThree = sideThree;
+    }
+
+    public bool IsValid()
+    {
+      return true;
+    }
   }
 }
